f047447b004ba4a22bad1fc6d08b174478c45ca1
GiorgioMarmolino <giorgio.marmolino@gmail.com>
2026-05-28 15:51:24 +0200

    Debug LaunchFile

--- src/navila_ros2_bridge/launch/navila.launch.py
+++ src/navila_ros2_bridge/launch/navila.launch.py
@@ -7,47 +7,50 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
     navila_package = 'navila_ros2_bridge'
+    inference_rate_hz = LaunchConfiguration("inference_rate_hz")
+    use_phi3 = LaunchConfiguration("use_phi3")
+    phi3_4bit = LaunchConfiguration("phi3_4bit")
 
-    navila_node = Node(
-        package=navila_package,
-        executable='navila_super_node',
-        name='navila_super_node',
-        output='screen',
-        parameters=[{
-            "use_sim_time":      True,
-            "use_phi3":          LaunchConfiguration("use_phi3"),
-            "inference_rate_hz": LaunchConfiguration("inference_rate_hz"),
-            "phi3_4bit":         LaunchConfiguration("phi3_4bit"),
-        }],
-    )
-
-    action_to_cmdvel_node = Node(
-        package=navila_package,
-        executable='action_to_cmdvel_node',
-        name='action_to_cmdvel_node',
-        output='log',
-        parameters=[{
-            "use_sim_time": True,
-        }],
-    )
-
-    instruction_node = ExecuteProcess(
-        cmd=[
-            'xterm', '-title', 'NaVILA Goal Input', '-e',
-            'bash -c "source /opt/ros/humble/setup.bash && '
-            'source /home/ros_ws/install/setup.bash && '
-            'ros2 run navila_ros2_bridge instruction_node; '
-            'echo DONE; read"'
-        ],
-        output='screen',
-    )
 
     return LaunchDescription([
         DeclareLaunchArgument("inference_rate_hz", default_value="1.0"),
         DeclareLaunchArgument("use_phi3",          default_value="false"),
         DeclareLaunchArgument("phi3_4bit",         default_value="false"),
-        SetEnvironmentVariable('ROS_DOMAIN_ID', '105'),
-        navila_node,
-        action_to_cmdvel_node,
-        instruction_node,
+        
+        #SetEnvironmentVariable('ROS_DOMAIN_ID', '105'),
+
+        Node(
+            package=navila_package,
+            executable='navila_super_node',
+            name='navila_super_node',
+            output='screen',
+            parameters=[{
+                "use_sim_time":      True,
+                "use_phi3":          use_phi3,
+                "inference_rate_hz": inference_rate_hz,
+                "phi3_4bit":         phi3_4bit,
+            }],
+        ),
+
+        Node(
+            package=navila_package,
+            executable='action_to_cmdvel_node',
+            name='action_to_cmdvel_node',
+            namespace='',
+            output='screen',
+            parameters=[{
+                "use_sim_time": True,
+            }],
+        ),
+
+        ExecuteProcess(
+            cmd=[
+                'xterm', '-title', 'NaVILA Goal Input', '-e',
+                'bash -c "source /opt/ros/humble/setup.bash && '
+                'source /home/ros_ws/install/setup.bash && '
+                'ros2 run navila_ros2_bridge instruction_node; '
+                'echo DONE; read"'
+            ],
+            output='screen',
+        ),
     ])
