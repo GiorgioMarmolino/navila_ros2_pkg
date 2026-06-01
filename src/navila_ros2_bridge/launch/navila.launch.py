50e2f07f37279fb3b0fe413ab3caf5783b7089c2
GiorgioMarmolino <giorgio.marmolino@gmail.com>
2026-06-01 15:43:45 +0200

    Update launch file

--- src/navila_ros2_bridge/launch/navila.launch.py
+++ src/navila_ros2_bridge/launch/navila.launch.py
@@ -1,5 +1,5 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, ExecuteProcess
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, TimerAction
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
@@ -8,30 +8,15 @@ def generate_launch_description():
 
     navila_package = 'navila_ros2_bridge'
     inference_rate_hz = LaunchConfiguration("inference_rate_hz")
-    use_phi3 = LaunchConfiguration("use_phi3")
-    phi3_4bit = LaunchConfiguration("phi3_4bit")
-
+    use_phi3          = LaunchConfiguration("use_phi3")
+    phi3_4bit         = LaunchConfiguration("phi3_4bit")
 
     return LaunchDescription([
         DeclareLaunchArgument("inference_rate_hz", default_value="1.0"),
         DeclareLaunchArgument("use_phi3",          default_value="false"),
         DeclareLaunchArgument("phi3_4bit",         default_value="false"),
-        
-        #SetEnvironmentVariable('ROS_DOMAIN_ID', '105'),
 
-        Node(
-            package=navila_package,
-            executable='navila_super_node',
-            name='navila_super_node',
-            output='screen',
-            parameters=[{
-                "use_sim_time":      True,
-                "use_phi3":          use_phi3,
-                "inference_rate_hz": inference_rate_hz,
-                "phi3_4bit":         phi3_4bit,
-            }],
-        ),
-
+        # Avvia subito action_to_cmdvel e instruction
         Node(
             package=navila_package,
             executable='action_to_cmdvel_node',
@@ -52,5 +37,24 @@ def generate_launch_description():
                 'echo DONE; read"'
             ],
             output='screen',
+        ),
+
+        # Start navila_super_node after 4 secs
+        TimerAction(
+            period=4.0,
+            actions=[
+                Node(
+                    package=navila_package,
+                    executable='navila_super_node',
+                    name='navila_super_node',
+                    output='screen',
+                    parameters=[{
+                        "use_sim_time":      True,
+                        "use_phi3":          use_phi3,
+                        "inference_rate_hz": inference_rate_hz,
+                        "phi3_4bit":         phi3_4bit,
+                    }],
+                ),
+            ]
         ),
     ])
